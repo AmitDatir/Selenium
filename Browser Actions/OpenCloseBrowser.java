25a519059fc73b6b2842b31f8f98257f1449486a
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-02-05 14:19:32 +0530

    Create OpenCloseBrowser.java

---
+++ Browser Actions/OpenCloseBrowser.java
@@ -0,0 +1,16 @@
+package Selenium_P01.T001;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+	public static void main(String args[]) throws Exception{
+		System.setProperty("webdriver.chrome.driver", "D:\\Automation\\Drivers\\Chrome\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Browser opened successfully.");
+		driver.get("https://www.google.com");
+		Thread.sleep(3000);
+		driver.close();
+		System.out.println("Browser closed successfully");
+	}
+}
