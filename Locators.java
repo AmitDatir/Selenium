fa65effe11680ca9583b1ac349c0ebf8df8d8912
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-10-22 12:59:31 +0530

    Update Locators.java

--- Locators.java
+++ Locators.java
@@ -39,13 +39,13 @@ WebElement element = driver.findElement(By.className("submit"));           //cla
 
 
 -Tag Name:
-WebElement element = driver.findElement(By.tagName("placeholder")); 
+WebElement element = driver.findElement(By.tagName("placeholder"));              //tagname should be uniue
 
 
 
 //<a href="#">Forgot your password</a>
 -Linked Text:
-WebElement element = driver.findElement(By.linkedText("Forgot your password")); 
+WebElement element = driver.findElement(By.linkedText("Forgot your password"));     
 
 
 
@@ -70,22 +70,38 @@ parentTag childTag             :             form h2
 
 //incomplete attribute value:
 input[type *= 'pass']        //complete value: password
-
+tagname                      //using only unique tagname also works for css
 
     
 --XPath:
 tagName[@attribute1 = 'value1'][@attribute2 = 'value2']                  //  //input[@placeholder = 'Username']
 
+Absolete xpath:
+/html/tagname/tagname                //absolete xpath always starts with single / & from root node that is html tag
+Relative xpath:
+//tagname/tagname                    //relative xpath starts from any relative location and it always starts with //
+
 //to check element using console : 
 $x('css locator')
 
 //how to handle case of multiple elements with same attribute values
-//input[@type = 'text'][2]                    //2 is the index of the desired locator
+    //input[@type = 'text'][2]                    //2 is the index of the desired locator
 
 //incomplete attribute value:
-//input[contains(@text,'pass')]        //complete value: password
+    //input[contains(@text,'pass')]        //complete value: password
+    //tagname                              //using only unique tagname also works for xpath
 
 //Parent-child traverse:                        // :- search within entire page   / :- search within stated parent tag only
-//parentTag/childTag                                   :            //form/h2
-//parentTag/childTag[index]                            :            //form/input[2]
-//parentTag[@attribute = 'value']/childTag[index]      :            //div[@class = 'forgot-pwd-btn-container']/button[2]
+    //parentTag/childTag                                   :            //form/h2
+    //parentTag/childTag[index]                            :            //form/input[2]
+    //parentTag[@attribute = 'value']/childTag[index]      :            //div[@class = 'forgot-pwd-btn-container']/button[2]
+    //parentXpath //childXpath                             :            //div[@id = 'sncks_sxinc_scjbc'] //a[@value = 'MAA']
+
+//Sibling traverse:
+    //parentTag/childTag/following-sibling::siblingTagName[index]                       //header/div/button[1]/following-sibling::button[1]
+
+//child to parent travrse:
+    //parentTag/childTag/childTag/parent::parentTagName                                //header/div/button[1]/parent::div
+
+//Xpath using locator text:
+    //tagname[text() = 'locator text']                   //button[text() = 'Log Out']
